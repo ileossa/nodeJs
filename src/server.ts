ae0bcf810a772e9425e05237398ec4e0b9fb9c76
Ileossa <4510253+ileossa@users.noreply.github.com>
2020-04-18 18:28:50 +0200

    Move configuration on `.env` file, use dotenv-> gestion properties. Mongoose gestion connection on mongoDB.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
 import App from './app'
+import * as mongoose from 'mongoose'
+import 'dotenv/config'
 import PostController from './services/posts/posts.controller'
 import PingController from './services/ping/ping.controller'
+
+
+const {
+    MONGO_USER,
+    MONGO_PASSWORD,
+    MONGO_PATH
+} = process.env
+
+mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`)
 
 const app = new App([new PostController(), new PingController()], 5000)
 app.listen()
