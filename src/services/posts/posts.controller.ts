210659f429f91dc03a643eef704b551780b4c4c4
Ileossa <4510253+ileossa@users.noreply.github.com>
2020-04-19 15:45:04 +0200

    now we can getAll, get specific post, save new post, delete if exist.

--- src/services/posts/posts.controller.ts
+++ src/services/posts/posts.controller.ts
@@ -1,17 +1,12 @@
 import * as express from 'express'
 import Post from './post.interface'
+import postModel from "./posts.model";
+import {request} from "express";
 
-class PostsController {
+export default class PostsController {
     public path = '/posts'
     public router = express.Router()
-
-    private posts: Post[] = [
-        {
-            author: 'Marcin',
-            content: 'Dolor sit amet',
-            title: 'Lorem Ipsum',
-        }
-    ]
+    private model = postModel
 
     constructor() {
         this.initializaRoutes()
@@ -19,17 +14,51 @@ class PostsController {
 
     public initializaRoutes() {
         this.router.get(this.path, this.getAllPosts)
+        this.router.get(`${this.path}/:id`, this.getPostById)
+        this.router.put(`${this.path}/:id`, this.modifyPost)
+        this.router.delete(`${this.path}/:id`, this.deletePost)
         this.router.post(this.path, this.createAPost)
     }
 
-    getAllPosts = (req: express.Request, res: express.Response) => {
-        res.send(this.posts)
+    private getAllPosts = (req: express.Request, res: express.Response) => {
+        this.model.find()
+            .then((postfind) => {
+                res.send(postfind)
+            })
     }
-    createAPost = (req: express.Request, res: express.Response) => {
-        const post: Post = req.body
-        this.posts.push(post)
-        res.send(post)
-    }
-}
 
-export default  PostsController
+    private getPostById = (req: express.Request, res: express.Response) => {
+        const id = req.params.id
+        this.model.findById(id)
+            .then((postfind) => {
+                res.send(postfind)
+            })
+    }
+
+    private modifyPost = (req: express.Request, res: express.Response) => {
+        const id = req.params.id
+        const input = req.body
+        this.model.findByIdAndUpdate(id, input, {new: true})
+            .then((postUpdate) => {
+                res.send(postUpdate)
+            })
+    }
+
+    private createAPost = (req: express.Request, res: express.Response) => {
+        const input: Post = req.body
+        const createdPost = new this.model(input)
+        createdPost.save()
+            .then((postCreated) => {
+                res.send(postCreated)
+            })
+    }
+
+    private deletePost = (req: express.Request, res: express.Response) => {
+        const id = request.params.id
+        this.model.findByIdAndDelete(id)
+            .then((successPostCreated) => {
+                successPostCreated ? res.send(200) : res.send(500)
+            })
+    }
+
+}
